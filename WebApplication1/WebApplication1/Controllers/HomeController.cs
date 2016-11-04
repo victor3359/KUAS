33321a47f4fcf2b1ec469b7bf5f3580774522d05
KUAS <user@TEACHER>
2016-11-04 12:05:27 +0800

    1104

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,30 +8,30 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        public class AlbumWithCount
+        {
+            public Models.Album Data { get; set; }
+            public int ReadCount { get; set; }
+        }
+
+
         // GET: Home
         public ActionResult Index(string id,string name,string title)
         {
             ViewBag.ID = id;
-            List<Models.Album> list = new List<Models.Album>();
+            Service.DataService data = new Service.DataService();
+            var list = data.LoadAllAlbum();
+            var result = list.ToList().Select(x =>
+              {
+                  var item = new AlbumWithCount();
+                  item.Data = x;
+                  item.ReadCount = 100;
+                  return item;
+              }).ToList();
 
+            //ViewBag.ReadCount = 0;
 
-            var item = new Models.Album();
-
-            item.Genre="pop";
-            item.Title = "The Best Of The Men At Work";
-            item.Price = 8;
-
-            list.Add(item);
-
-            var item2 = new Models.Album();
-
-            item2.Genre = "Metal";
-            item2.Title = "Black Light Syndrome";
-            item2.Price = 20;
-            list.Add(item2);
-            ViewBag.ListDatas = list;
-
-            return View();
+            return View(result);
             //return Content("<h1>hello world</h1>" + id);
         }
     }
